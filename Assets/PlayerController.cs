5e4072219b7ba249fd7bacba8556f500e2838226
RichardOhata <richardohata@gmail.com>
2025-01-25 02:32:43 -0800

    Added mouse position rotation

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour
     public PlayerControls controls;
 
     Vector2 moveDirection = Vector2.zero;
-
+    Vector2 mousePositon = Vector2.zero;
     private void Awake()
     {
         controls = new PlayerControls();
@@ -17,27 +17,33 @@ public class PlayerController : MonoBehaviour
     private void OnEnable()
     {
         controls.Player.Movement.Enable();
+        controls.Player.MousePosition.Enable();
     }
 
     private void OnDisable()
     {
         controls.Player.Movement.Disable();
-    }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+        controls.Player.MousePosition.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
         moveDirection = controls.Player.Movement.ReadValue<Vector2>();
+        mousePositon = controls.Player.MousePosition.ReadValue<Vector2>();
     }
 
     private void FixedUpdate()
     {
         rb.linearVelocity = moveDirection * moveSpeed;
+        RotatePosition();
+    }
+
+    private void RotatePosition()
+    {
+        mousePositon = Camera.main.ScreenToWorldPoint(mousePositon);
+        Vector2 direction = new Vector2(mousePositon.x - transform.position.x, mousePositon.y - transform.position.y);
+        transform.up = direction;
+
     }
 }
