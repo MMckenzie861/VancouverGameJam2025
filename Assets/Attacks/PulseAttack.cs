279bc3cf3bfe5b9c0cb90063e493728ad081dae0
Shuang Wu <shuang111994@gmail.com>
2025-01-25 20:16:41 -0800

    fix pulse atk trigger enter 2d

--- Assets/Attacks/PulseAttack.cs
+++ Assets/Attacks/PulseAttack.cs
@@ -66,7 +66,19 @@ public class PulseAttack : MonoBehaviour
     /// collider (2D physics only).
     /// </summary>
     /// <param name="other">The Collision2D data associated with this collision.</param>
-    void OnCollisionEnter2D(Collision2D other)
+    // void OnTriggerEnter2D(Collision2D other)
+    // {
+        // if (other.gameObject.CompareTag("Orbital")) {
+        //     Destroy(gameObject);
+        // }
+    // }
+
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Orbital")) {
             Destroy(gameObject);
