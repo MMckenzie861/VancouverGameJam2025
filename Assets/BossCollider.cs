4f47694c89d38e929cd2f0392525943c2682d0f9
TrevorHong <65141699+TrevorHong@users.noreply.github.com>
2025-01-25 20:37:40 -0800

    pull again

--- Assets/BossCollider.cs
+++ Assets/BossCollider.cs
@@ -5,10 +5,6 @@ public class BossCollider : MonoBehaviour
     public GameManager gameManager;
 
     public float damage = 1;
-
-    public void Start() {
-        gameManager = GameObject.FindGameObjectWithTag("Manager").GetComponent<GameManager>();
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Orbital"))
